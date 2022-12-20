f892a0b54c8e318487f77d587090382fb6008fb6
Igor Martinez <igorgabriel.martinez@gmail.com>
2022-12-19 22:12:57 -0300

    Add project create at Spring Initializr

---
+++ api/src/test/java/br/com/igormartinez/restapiwithspringboot/RestApiWithSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.igormartinez.restapiwithspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiWithSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
