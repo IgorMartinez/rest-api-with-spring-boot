46cf3c5f70520950dd18d9c788021843cf9a2f49
Igor Martinez <igorgabriel.martinez@gmail.com>
2022-12-21 19:36:11 -0300

    Add Flyway (migrations) support

---
+++ api/src/main/resources/db/migration/V2__Insert_table_person.sql
@@ -0,0 +1,5 @@
+INSERT INTO `restapiwithspringboot`.`person`(`first_name`,`last_name`,`address`,`gender`)
+VALUES
+('Lionel','Messi','Argentina','M'),
+('Andres','Iniesta','Espanha','M'),
+('Cristiano','Ronaldo','Portugal','M');
