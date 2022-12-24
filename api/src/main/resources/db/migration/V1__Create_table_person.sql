2a0eac7355bcdad8478b6081660051f49c858378
Igor Martinez <igorgabriel.martinez@gmail.com>
2022-12-24 11:57:26 -0300

    Adjust migrations

--- api/src/main/resources/db/migration/V1__Create_table_person.sql
+++ api/src/main/resources/db/migration/V1__Create_table_person.sql
@@ -1,8 +1,8 @@
-CREATE TABLE `person` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `first_name` varchar(255) NOT NULL,
-  `last_name` varchar(255) NOT NULL,
-  `address` varchar(255) NOT NULL,
-  `gender` varchar(1) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+CREATE TABLE person (
+  id bigint NOT NULL AUTO_INCREMENT,
+  first_name varchar(255) NOT NULL,
+  last_name varchar(255) NOT NULL,
+  address varchar(255) NOT NULL,
+  gender varchar(1) DEFAULT NULL,
+  PRIMARY KEY (id)
 );
--- api/src/main/resources/db/migration/V2__Insert_table_person.sql
+++ api/src/main/resources/db/migration/V2__Insert_table_person.sql
@@ -1,4 +1,4 @@
-INSERT INTO `restapiwithspringboot`.`person`(`first_name`,`last_name`,`address`,`gender`)
+INSERT INTO person(first_name,last_name,address,gender)
 VALUES
 ('Lionel','Messi','Argentina','M'),
 ('Andres','Iniesta','Espanha','M'),
