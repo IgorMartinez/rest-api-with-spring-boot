15b066ec41d212b486dcebad514179ec19371db9
Igor Martinez <igorgabriel.martinez@gmail.com>
2023-05-31 20:25:06 -0300

    Add log in YAMLMapper

--- api/src/test/java/br/com/igormartinez/restapiwithspringboot/integrationtests/controller/withyaml/mapper/YAMLMapper.java
+++ api/src/test/java/br/com/igormartinez/restapiwithspringboot/integrationtests/controller/withyaml/mapper/YAMLMapper.java
@@ -1,5 +1,7 @@
 package br.com.igormartinez.restapiwithspringboot.integrationtests.controller.withyaml.mapper;
 
+import java.util.logging.Logger;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JsonMappingException;
@@ -11,6 +13,8 @@ import io.restassured.mapper.ObjectMapperDeserializationContext;
 import io.restassured.mapper.ObjectMapperSerializationContext;
 
 public class YAMLMapper implements ObjectMapper {
+
+    private Logger logger = Logger.getLogger(YAMLMapper.class.getName());
 
     private com.fasterxml.jackson.databind.ObjectMapper objectMapper;
     protected TypeFactory typeFactory;
@@ -27,10 +31,15 @@ public class YAMLMapper implements ObjectMapper {
         try {
             String dataToDeserialize = context.getDataToDeserialize().asString();
             Class type = (Class) context.getType();
+
+            logger.info("Trying deserialize object of type " + type);
+
             return objectMapper.readValue(dataToDeserialize, typeFactory.constructType(type));
         } catch (JsonMappingException e) {
+            logger.severe("Error deserializing object");
             e.printStackTrace();
         } catch (JsonProcessingException e) {
+            logger.severe("Error deserializing object");
             e.printStackTrace();
         }
         return null;
@@ -39,8 +48,10 @@ public class YAMLMapper implements ObjectMapper {
     @Override
     public Object serialize(ObjectMapperSerializationContext context) {
         try {
+            logger.info("Trying serialize object");
             return objectMapper.writeValueAsString(context.getObjectToSerialize());
         } catch (JsonProcessingException e) {
+            logger.severe("Error serializing object");
             e.printStackTrace();
         }
         return null;
